0c9890d449b14d9799fe6c18f7656ba3bc4802a7
DaedricMD <dragonborn2649@gmail.com>
2016-10-29 14:16:30 -0400

    it actually has a clock now

--- src/c/main.c
+++ src/c/main.c
@@ -25,6 +25,24 @@ static void main_window_unload(Window *window) {
  text_layer_destroy(s_time_layer);
 }
 
+static void update_time() {
+  // Get a tm structure
+  time_t temp = time(NULL);
+  struct tm *tick_time = localtime(&temp);
+
+  // Write the current hours and minutes into a buffer
+  static char s_buffer[8];
+  strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ?
+                                          "%H:%M" : "%I:%M", tick_time);
+
+  // Display this time on the TextLayer
+  text_layer_set_text(s_time_layer, s_buffer);
+}
+
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+  update_time();
+}
+
 static void init() {
   s_main_window = window_create();
   
@@ -34,6 +52,9 @@ static void init() {
   });
   
   window_stack_push(s_main_window, true);
+  
+  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+  update_time();
   
 }
 
