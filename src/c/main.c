281ed7ddcbdf2a0f1b34c3ab489f82fbe38b7ced
DaedricMD <dragonborn2649@gmail.com>
2016-10-29 12:22:14 -0400

    beginning code

--- src/c/main.c
+++ src/c/main.c
@@ -1,22 +1,32 @@
 #include <pebble.h>
+static Window *s_main_window;
 
-Window *my_window;
-TextLayer *text_layer;
+static void init() {
+  
+  static void main_window_load(Window *window) {
 
-void handle_init(void) {
-  my_window = window_create();
+  }
+  static void main_window_unload(Window *window) {
 
-  text_layer = text_layer_create(GRect(0, 0, 144, 20));
-  window_stack_push(my_window, true);
+  }
+  
+  s_main_window = window_create();
+  
+  window_set_window_handlers(s_main_window, (WindowHandlers) {
+    .load = main_window_load,
+    .unload = main_window_unload
+  });
+  
+  window_stack_push(s_main_window, true);
+  
 }
 
-void handle_deinit(void) {
-  text_layer_destroy(text_layer);
-  window_destroy(my_window);
+static void deinit() {
+window_destroy(s_main_window);
 }
 
 int main(void) {
-  handle_init();
+  init();
   app_event_loop();
-  handle_deinit();
+  deinit();
 }
